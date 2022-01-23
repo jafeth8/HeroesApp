f3cb793739bdbdddb588c04f4d00ec76fc166ed3
Jafeth8 <romeojafet@gmail.com>
2022-01-23 10:22:29 -0600

    modificado primer route: estableciendo la clase activa

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+
+    const handleLogout = () => {
+
+        // TODO: por hacer
+        console.log("salir");
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
@@ -16,18 +23,17 @@ export const Navbar = () => {
                 <div className="navbar-nav">
 
                     <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
+                        className={({isActive})=> "nav-item nav-link " + (isActive ? "active" : "")} 
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
                     <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
+                        
+                        //className="nav-item nav-link"
+                        /*aunque la clase isActive se detecta automaticamente esta metodologia se puede usar para poner otras clases aparte de la clase ya mencionanda */
+                        className={({isActive})=> "nav-item nav-link " + (isActive ? "active" : "")}  
                         to="/dc"
                     >
                         DC
@@ -35,16 +41,19 @@ export const Navbar = () => {
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
+
+                    <span className="nav-item nav-link text-info">
+                        Romeo
+                    </span>
+
+                    <button 
+                        className="nav-item nav-link btn"
+                        onClick={handleLogout} 
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
