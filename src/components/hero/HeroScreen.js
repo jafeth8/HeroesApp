007b4a6e80abf53e94d74be12ac04c251f77aa64
Jafeth8 <romeojafet@gmail.com>
2022-01-29 10:07:18 -0600

    estilizando  el componente heroScreen
    uso de useNavigate del paquete react-router-dom para regresar a pagina anterior

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate,useNavigate, useParams } from 'react-router-dom';
 import { getHeroesById } from '../../selectors/getHeroesById';
 
 export const HeroScreen = () => {
 
     const {heroeId} = useParams();
 
+    const navigate=useNavigate()
+
     //console.log(heroeId);
     const hero=getHeroesById(heroeId)
+
+    const handleReturn = () => {
+
+        navigate( -1 );//-1 como parametro: regresa a la pagina anterior   
+
+    }
 
     if(!hero){
 
@@ -15,10 +23,47 @@ export const HeroScreen = () => {
         
     }
     
+    const {
+        id,
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters
+    } = hero;
+
+    const imagePath = `/assets/${ id }.jpg`;
+
     return (
-        <div>
-            <h1>HeroScreen</h1>
-            <p>{hero.superhero}</p>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img 
+                    src={ imagePath } 
+                    alt={ superhero }
+                    className="img-thumbnail animate__animated animate__fadeInLeft"
+                />
+            </div>
+
+            <div className="col-8 animate__animated animate__fadeIn">
+                <h3>{ hero.superhero }</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"> <b>Alter ego:</b> { alter_ego } </li>
+                    <li className="list-group-item"> <b>Publisher:</b> { publisher } </li>
+                    <li className="list-group-item"> <b>First Appearance:</b> { first_appearance } </li>
+                </ul>
+
+                <h5 className="mt-3">Characters</h5>
+                <p>{ characters }</p>
+
+                <button
+                    className="btn btn-outline-info"
+                    onClick={ handleReturn }
+                >
+                    Regresar 
+                </button>
+
+            </div>
+
         </div>
     );
 };
