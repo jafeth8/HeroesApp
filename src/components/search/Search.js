030ca95c043656b6714ff2ab76bfacbcb91c51d2
Jafeth8 <romeojafet@gmail.com>
2022-01-30 11:42:26 -0600

    searcHComponent: uso de custom hook 'useForm'

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,9 +1,50 @@
 import React from 'react';
 
+import {useForm} from '../../hooks/useForm'
+
 export const SearchScreen = () => {
+
+    const [formValues,handleInputChange]=useForm({searchText:''})
+
+    const {searchText}=formValues;
+    
+    const handleSearch=(e)=>{
+        e.preventDefault();
+        console.log(searchText);
+    }
+    
+
     return (
-        <div>
-            SearchScreen
-        </div>
+        <>
+            <h1>Búsquedas</h1>
+            <hr />
+
+            <div className="row">
+
+                <div className="col-5">
+                    <h4>Buscar</h4>
+                    <hr />
+
+                    <form onSubmit={ handleSearch }>
+                        <input 
+                            type="text"
+                            placeholder="Buscar un héroe"
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
+                            value={ searchText }
+                            onChange={ handleInputChange }
+                        />
+
+                        <button 
+                            className="btn btn-outline-primary mt-1"
+                            type="submit">
+                            Buscar...
+                        </button>
+
+                    </form>
+                </div>
+            </div>    
+        </>
     )
 };
